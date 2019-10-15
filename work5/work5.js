66fdd0ade22526b041649b78be19002b418e106b
a1216154520 <1216154520@qq.com>
2019-10-15 15:50:42 +0800

    work6

--- work5/work5.js
+++ work5/work5.js
@@ -1,14 +1,13 @@
-
- str = '<table border="1">'
- for(let i=1;i<10;++i){
-     str +='<tr>'
-     for(var j=1;j<=i;++j){
-         str +='<td>&nbsp;</td>'
-     }
-     str += '</tr>'}
- 
-     str +='</table>'
- document.getElementById("table1").innerHTML =str
+str1 = "<table border= '1'>"
+for(let i=9;i>0;--i){
+    str1 += "<tr>"
+    for(var j=9;j>0;--j){
+        str1 += "<td>&nbsp;</td>"
+    }
+    str1 += "</tr>"
+}
+str1 += "</table>"
+document.getElementById("table1").innerHTML = str1;
  
  str ='<table border="1">'
  for(let i=9;i>0;--i){
---
+++ work6/pai.js
@@ -0,0 +1,32 @@
+function calculate(){
+    let num = document.getElementById('***').nodeValue
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById(***).value = pi(**)
+    
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1,i>=num;++i){
+        s***
+    }
+
+ return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1,i=num;++i){
+        s*****
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial(****)
+        let divisor = oddFactorial(*****)
+        s****
+    }
+    retu sum*****
+}
