66e7fd33e00bf3056139a2adbbd10857b9027a38
a1216154520 <1216154520Wqq.com>
2019-10-16 21:32:46 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,32 +1,32 @@
 function calculate(){
-    let num = document.getElementById('***').nodeValue
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById(***).value = pi(**)
-    
+        document.getElementById('pai').value = pi(num)
     }
 }
 function factorial(n){
     let sum = 1
-    for(let i=1,i>=num;++i){
-        s***
+    for(let i=1;i<=n;++i){
+        sum *=i
     }
-
- return sum
+    return sum
 }
 function oddFactorial(n){
     let sum = 1
-    for(let i=1,i=num;++i){
-        s*****
+    for (let i=1;i<=n;++i){
+        sum *= 2*i+1
+
     }
     return sum
 }
 function pi(num){
     let sum = 0
-    for(let i=0;i<=num;++i){
-        let dividend = factorial(****)
-        let divisor = oddFactorial(*****)
-        s****
+    for (let i=0;i<=num;++i){
+        let dividend =factorial(i)
+        let divisor =oddFactorial(i)
+        sum +=dividend/divisor
     }
-    retu sum*****
+    return sum *2
 }
+calculate()
